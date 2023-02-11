c4fb8c1d656fdeacc9c6ac96f30b5303823408b1
toyama <shouya.toyama@gmail.com>
2023-02-11 22:14:55 +0900

    fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-docker build -t $DOCKER_IMAGE:$DOCKER_TAG .
+ALIAS=$DOCKER_USER/$DOCKER_IMAGE
+
+docker build -t $ALIAS:$DOCKER_TAG .
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-docker push -t $DOCKER_IMAGE:$DOCKER_TAG
+docker push $ALIAS:$DOCKER_TAG
